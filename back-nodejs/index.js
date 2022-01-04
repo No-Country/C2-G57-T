a9e16c3019e3b9d96ff9f5a4cd0b06400e775cab
Mauricio <mauri.yapura@yahoo.com>
2022-01-04 10:28:16 -0300

    Modelo de servidor

--- back-nodejs/index.js
+++ back-nodejs/index.js
@@ -1,12 +1,6 @@
-const express = require(`express`)
-const app = express()
 
-//settingss
-app.set("port", 3000)
-app.get("/", (req, res) => {
-    res.send("Bienvenido Wey")
-})
-app.listen(app.get("port"), () => {
-    console.log(`la aplicaison esta corriendo en el puerto ${app.get('port')}`)
+const Server = require("./models/server");
 
-})
+const server = new Server();
+
+server.serverStart();
