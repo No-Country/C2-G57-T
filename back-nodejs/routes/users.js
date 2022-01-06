a5929a41d226595f9ee3b3c3168943deaa51ec80
Mauricio <mauri.yapura@yahoo.com>
2022-01-06 20:26:56 -0300

    avance de crud de usuarios. Modelo de producto agregado

--- back-nodejs/routes/users.js
+++ back-nodejs/routes/users.js
@@ -1,18 +1,15 @@
 
 const { Router } = require("express");
-const { postUser, deleteUser } = require("../controllers/users");
+const { postUser, deleteUser, putUser } = require("../controllers/users");
 
 const router = Router();
 
-
-
-
 router.post("/", postUser);
 
+router.put("/:id", putUser);
 
 router.delete("/:id", deleteUser);
 
 
 
 module.exports = router; 
-
