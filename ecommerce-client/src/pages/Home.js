2127701b7c7c16b00cc067321daa0c5fdaa2ffca
MichelVarela <michelvarela.95@outlook.com>
2022-01-07 22:35:29 -0300

    second commit

--- ecommerce-client/src/pages/Home.js
+++ ecommerce-client/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Carousel from '../components/Carousel';
 
 const Home = () => {
   return (
     <div className='container__page'>
-      <h1>Home</h1>
+      <Carousel/>
     </div>
   );
 };
