f57301a4e968c3e506ce25797161d65ce63a8417
adrianadducchio <adrian.vhvhvh@gmail.com>
2022-01-08 13:29:50 -0300

    control de formularios + conexion backend

--- ecommerce-client/src/components/LoginInputs.js
+++ ecommerce-client/src/components/LoginInputs.js
@@ -1,48 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "./../hooks/useForm";
+import { clientAxios } from "./../config/axios";
 
 const LoginInputs = () => {
+  const [error, setError] = useState(false);
+
   const { values, handleInputChange, reset } = useForm({
     email: "",
     password: "",
   });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("valueslogin", values);
+    try {
+      const { data } = await clientAxios.post("api/auth/login", values);
+
+      console.log("resp", data);
+
+      localStorage.setItem("token", data.token);
+    } catch (error) {
+      setError(true);
+
+      setTimeout(() => {
+        setError(false);
+      }, 3000);
+    }
+
     reset();
   };
 
   return (
-    <form
-      autoComplete='off'
-      className='login__containerInput'
-      onSubmit={handleSubmit}
-    >
-      <input
-        name='email'
-        onChange={handleInputChange}
-        placeholder='Email'
-        type='email'
-        value={values.email}
-      />
-      <input
-        name='password'
-        onChange={handleInputChange}
-        placeholder='Contraseña'
-        type='password'
-        value={values.password}
-      />
-      <div className='login__forgotPass'>
-        <div className='checkboxContainer'>
-          <input type='checkbox' className='checkbox' />
-          <label>Recordar</label>
+    
+      <form
+        autoComplete='off'
+        className='login__containerInput'
+        onSubmit={handleSubmit}
+      >
+      {error && <p className="banner__error">Usuario o Contraseña incorrecta</p>}
+        <input
+          name='email'
+          onChange={handleInputChange}
+          placeholder='Email'
+          type='email'
+          value={values.email}
+        />
+        <input
+          name='password'
+          onChange={handleInputChange}
+          placeholder='Contraseña'
+          type='password'
+          value={values.password}
+        />
+        <div className='login__forgotPass'>
+          <div className='checkboxContainer'>
+            <input type='checkbox' className='checkbox' />
+            <label>Recordar</label>
+          </div>
+          <Link to='#'>Olvidaste la contraseña?</Link>
         </div>
-        <Link to='#'>Olvidaste la contraseña?</Link>
-      </div>
-      <input type='submit' className='login__inputSubmit' value='ingresar' />
-    </form>
+        <input type='submit' className='login__inputSubmit' value='ingresar' />
+      </form>
+    
   );
 };
 
--- ecommerce-client/src/config/axios.js
+++ ecommerce-client/src/config/axios.js
@@ -3,3 +3,4 @@ import axios from "axios"
 export const clientAxios = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL
 })
+
